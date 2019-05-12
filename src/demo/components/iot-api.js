0351226995b9e3113e454532b09793436feb9e75
Glitch (cs1) <none>
2019-05-12 19:39:31 +0000

    update iot-component in demo

--- src/demo/components/iot-api.js
+++ src/demo/components/iot-api.js
@@ -2,12 +2,12 @@ export default CS1=>{
   
   AFRAME.registerComponent('iot-api', {
     schema:{
-      led:{type:'string',default:'on'}
+      LED:{type:'string',default:'on'}
     },
     init: function () {   
       const self = this;
       this.el.addEventListener('click',e=>{
-        console.log('Click detected on led.');
+        console.log('Click detected on LED.');
         self.toggleLED();
       });       
     },
@@ -16,14 +16,14 @@ export default CS1=>{
     },
     toggleLED: function () {
       let mat;
-      if(this.data.led == 'on'){
+      if(this.data.LED == 'on'){
         mat = 'color:#680c1a;emissiveIntensity:0';
-        this.data.led = 'off';
-        CS1.db.set({led:'off'},()=>{});
+        this.data.LED = 'off';
+        CS1.db.set({LED:'off'},()=>{});
       }else{   
         mat = 'color:red;emissiveIntensity:4'; 
-        this.data.led = 'on';
-        CS1.db.set({led:'on'},()=>{});
+        this.data.LED = 'on';
+        CS1.db.set({LED:'on'},()=>{});
       }
       this.el.setAttribute('material',mat);
        
