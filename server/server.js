f6301c46e3dc0f633657426725a73b699ecc68a8
Glitch (clean-start) <none>
2019-11-24 00:31:17 +0000

    v0.6.96

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(function(req, res, next) {
       if ((req.get('X-Forwarded-Proto') !== 'https')) {
         res.redirect('https://' + req.get('Host') + req.url);
       } else
-        next();
+        next(); 
     });
 app.get("/", function (request, response) {
   response.sendFile('./public/index.html',{root:'.'});
