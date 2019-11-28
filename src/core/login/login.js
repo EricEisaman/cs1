35af6f9721c7ddbf74a5058ff61c918bd4aa5841
Glitch (cs1) <none>
2019-11-28 19:06:48 +0000

    login update, offline support added

--- src/core/login/login.js
+++ src/core/login/login.js
@@ -1,7 +1,7 @@
-import config from '../../../.data/client-config.json';
-export default(()=>{
-window.onload = e=>{
-  let loginHTML = `
+import config from "../../../.data/client-config.json";
+export default (() => {
+  window.onload = e => {
+    let loginHTML = `
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: block;background-color:${config.theme.overlayColor}">
     <div class="modal-dialog">
 		  <div class="loginmodal-container" style="background-color:${config.theme.formColor}">
@@ -17,41 +17,53 @@ window.onload = e=>{
 			</div>
 	</div>
 </div>
-`
- let loginContainer = document.querySelector('#login');
- loginContainer.innerHTML = loginHTML;
- setTimeout(e=>{
-   document.querySelector('.q1').focus(); 
-   if(!(CS1 && CS1.socket.connected)){
-     document.getElementById('login-msg').innerHTML = 'OFFLINE MODE';
-      document.querySelector('#lb').setAttribute('value','Play Game Offline');
-   }
- },1000);
- document.getElementById('lb').addEventListener("click",login);
- document.querySelector('.q2').addEventListener("keydown",e=>{
-   if(e.code=='Enter')login(e);
- }); 
-  
-  function login(e){
-    e.preventDefault(); 
-    if(!(CS1 && CS1.socket.connected)){
-      loginContainer.style.zIndex = -1;
-      loginContainer.setAttribute('hidden','');
-      CS1.myPlayer.components["movement-controls"].data.speed=CS1.myPlayer.startSpeed;
-      CS1.sounds.playerJoined.play();
-      setTimeout(()=>{CS1.say(CS1.game.announcements.welcome)},CS1.game.welcomeDelay);    
+`;
+    let loginContainer = document.querySelector("#login");
+    loginContainer.innerHTML = loginHTML;
+    document.querySelector("a-scene").addEventListener("loaded", function() {
+      document.querySelector(".q1").focus();
+      if (!(CS1 && CS1.socket.connected)) {
+        document.getElementById("login-msg").innerHTML = "OFFLINE MODE";
+        document
+          .querySelector("#lb")
+          .setAttribute("value", "Play Game Offline");
+      }
+    });
+    document.getElementById("lb").addEventListener("click", login);
+    document.querySelector(".q2").addEventListener("keydown", e => {
+      if (e.code == "Enter") login(e);
+    });
+
+    function login(e) {
+      e.preventDefault();
+      if (!(CS1 && CS1.socket.connected)) {
+        if (CS1 && !CS1.socket.connected) {
+          CS1.socket.close();
+        }
+        loginContainer.style.zIndex = -1;
+        loginContainer.setAttribute("hidden", "");
+        CS1.myPlayer.components.player.setSpeed(0.2);
+        CS1.game.start();
+        CS1.sounds.playerJoined.play();
+        setTimeout(() => {
+          CS1.say(CS1.game.announcements.welcome);
+        }, CS1.game.welcomeDelay);
+      } else if (
+        document.querySelector(".q1").value.length > 0 &&
+        document.querySelector(".q2").value.length > 0
+      ) {
+        if (navigator.vendor.includes("Apple")) {
+          CS1.sounds.playerJoined.play().catch(err => {
+            console.log(err);
+          });
+        }
+        CS1.login(
+          document.querySelector(".q1").value,
+          document.querySelector(".q2").value
+        );
+        document.querySelector(".q1").value = "";
+        document.querySelector(".q2").value = "";
+      }
     }
-    else if(document.querySelector('.q1').value.length > 0 && document.querySelector('.q2').value.length > 0){
-     if(navigator.vendor.includes('Apple')){
-        CS1.sounds.playerJoined.play()
-         .catch(err=>{console.log(err)});
-      } 
-     CS1.login(document.querySelector('.q1').value,document.querySelector('.q2').value);
-      document.querySelector('.q1').value = '';
-      document.querySelector('.q2').value = '';
-    }
-  }
-  
-}
-  
-})()
+  };
+})();
