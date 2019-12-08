d2eab0a66f8794368356414dc418a3826423fd2b
Glitch (devices2) <none>
2019-12-08 23:54:32 +0000

    v0.7.12  jump with all devices

--- src/aps/particles.js
+++ src/aps/particles.js
@@ -17,26 +17,25 @@ export default(()=>{
       CS1.particles={};
       CS1.particles.dust = Particles_Dust;
       CS1.particles.energy = Particles_Energy;
-      CS1.particles.orb = Particles_Orb;
+      CS1.particles.star = Particles_Energy;
       
       const energyImg = document.createElement('img');
       energyImg.setAttribute('crossorigin','anonymous');
       energyImg.src = 'https://cdn.glitch.com/f8abb766-9950-44ff-9adb-2f5f53fdaf1b%2Fblue_dart.png';
       energyImg.id = 'energy';
-      const assets = document.querySelector('a-assets');
-      assets.appendChild(energyImg);
+      document.body.appendChild(energyImg);
       
       const dustImg = document.createElement('img');
       dustImg.setAttribute('crossorigin','anonymous');
       dustImg.src = 'https://cdn.glitch.com/f8abb766-9950-44ff-9adb-2f5f53fdaf1b%2Fdust.png';
       dustImg.id = 'dust';
-      assets.appendChild(dustImg);
+      document.body.appendChild(dustImg);
       
-      const orbImg = document.createElement('img');
-      orbImg.setAttribute('crossorigin','anonymous');
-      orbImg.src = 'https://cdn.glitch.com/f8abb766-9950-44ff-9adb-2f5f53fdaf1b%2Fblue_dart.png';
-      orbImg.id = 'orb';
-      assets.appendChild(orbImg);
+      const starImg = document.createElement('img');
+      starImg.setAttribute('crossorigin','anonymous');
+      starImg.src = 'https://cdn.glitch.com/f8abb766-9950-44ff-9adb-2f5f53fdaf1b%2Fstar.png';
+      starImg.id = 'star';
+      document.body.appendChild(starImg);
 
       
     }
@@ -44,7 +43,7 @@ export default(()=>{
   
   AFRAME.registerComponent('particles', {
     schema: {
-      type: {type:'string',default:'energy'},
+      types: {type:'array'},
       color: {type:'string',default:'white'},
       scale: {type:'number',default:1},
       loop: {type:'boolean',default:false},
@@ -55,9 +54,17 @@ export default(()=>{
       this.e = document.createElement('a-entity');
       this.e.object3D.position.copy(this.el.object3D.position).add(this.data.offset);
       this.e.setAttribute('scale','3 3 3');
-      this.p = document.createElement('a-entity');
-      this.p.setAttribute('particleplayer',`src:${this.data.type}; img:#${this.data.type}; dur: 1000; count: 40%; scale: ${this.data.scale}; pscale: 2; interpolate: false; shader: standard; poolSize: 1; loop: ${this.data.loop}`);
-      this.e.appendChild(this.p);
+      this.types = {};
+      if(!this.data.types.length)this.data.types = ['energy','dust','star'];
+      
+      this.data.types.forEach(type=>{
+        
+      this.types[type] = document.createElement('a-entity');
+      this.types[type].setAttribute('particleplayer',`src:${type}; img:#${type}; dur: 1000; count: 40%; scale: ${this.data.scale}; pscale: 2; interpolate: false; shader: standard; poolSize: 1; loop: ${this.data.loop}`);
+      this.e.appendChild(this.types[type]);
+        
+      });
+      
       CS1.scene.appendChild(this.e);
           
       
@@ -65,13 +72,13 @@ export default(()=>{
     tick: function(t,dt){
       if(this.data.loop)this.e.object3D.position.copy(this.el.object3D.position).add(this.data.offset);
     },
-    fire: function(){
+    fire: function(type){
       this.e.object3D.position.copy(this.el.object3D.position).add(this.data.offset);
-      this.p.components.particleplayer.start();
+      if(this.types[type])this.types[type].components.particleplayer.start();
     },
-    fireAt: function(entity){
-      this.e.object3D.position.copy(entity.object3D.position).add(this.data.offset);
-      this.p.components.particleplayer.start();
+    fireAt: function(type,pos){
+      this.e.object3D.position.set(pos.x,pos.y,pos.z).add(this.data.offset);
+      if(this.types[type])this.types[type].components.particleplayer.start();
     }
     
   });
